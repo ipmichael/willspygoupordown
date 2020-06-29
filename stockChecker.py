94a04d4be25988eb744fed4e35d56765546f40c3
Michael Ip <michael@eaiti.com>
2020-06-28 23:08:24 -0400

    Weighted values for indicators

--- stockChecker.py
+++ stockChecker.py
@@ -7,36 +7,11 @@ import matplotlib.pyplot as plt
 from click._compat import raw_input
 import pandas as pd
 
-def connect(name, ticker, tickerprice, value):
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
-
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-		
-        # create a cursor
-        cur = conn.cursor()
-        
-	# execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('insert into public.indicators ')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-       
-	    # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
+def writeToDB(cursor, name, ticker, tickerprice, value):
+    """ Write to the PostgreSQL database server """
+    sql = f'insert into public.indicators(name,ticker,tickerprice,value) VALUES(\'{name}\',\'{ticker}\',{tickerprice},{value});'
+    print(sql)
+    cursor.execute(sql)
 
 class TechnicalIndicators:
     def __init__(self):
@@ -57,9 +32,9 @@ class TechnicalIndicators:
         a = TechIndicators(key=self.api_key2, output_format='json')
         data, meta_data = a.get_macd(symbol=self.stock_name,interval='daily')
         return data
-    def rsi(self):
-        b=TechIndicators(key=self.api_key2,output_format='json')
-        data,meta_data = b.get_rsi(symbol=self.stock_name,interval='daily',time_period=14)
+    def rsi(self, tp):
+        b=TechIndicators(key=self.api_key3,output_format='json')
+        data,meta_data = b.get_rsi(symbol=self.stock_name,interval='1min',time_period=tp,series_type='close')
         return data
     def bbands (self):
         c=TechIndicators(key=self.api_key2,output_format='json')
@@ -77,16 +52,59 @@ class TechnicalIndicators:
         z=TimeSeries(key=self.api_key1,output_format='json')
         data,meta_data=z.get_quote_endpoint(symbol=self.stock_name)
         return data
-if __name__ == "__main__":
-    TI=TechnicalIndicators()
-    # current_data = TI.current_data
-    # print(current_data['01. symbol'])
-    # print(current_data['05. price'])
-    ema50 = TI.ema(50)
-    ema20 = TI.ema(20)
-    ema20value = ema20[next(iter(ema20))]['EMA']
-    ema50value = ema50[next(iter(ema50))]['EMA']
+    def getStockName(self):
+        return self.stock_name
 
-    # print the most recent value in a time series
-    print(ema50value)
-    # print(ema_data)
+if __name__ == "__main__":
+    conn = None
+    TI=TechnicalIndicators()
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # getLastTickerpriceSQL = f'select tickerprice from public.indicators where ticker = \'{TI.getStockName()}\' order by time desc limit 1;'
+        # print(getLastTickerpriceSQL)
+        # cur.execute(getLastTickerpriceSQL)
+        # lastTickerprice = cur.fetchone()
+        # print(f'Last tickerprice for {TI.getStockName()}: {lastTickerprice}')
+
+        current_data = TI.current()
+        symbol = current_data['01. symbol']
+        currentPrice = float(current_data['05. price'])
+
+        ema50 = TI.ema(50)
+        ema20 = TI.ema(20)
+        ema20value = float(ema20[next(iter(ema20))]['EMA'])
+        ema50value = float(ema50[next(iter(ema50))]['EMA'])
+        emaPrediction = round((ema20value-ema50value)*10000/currentPrice,2)
+
+        writeToDB(cur, "EMA", symbol, currentPrice, emaPrediction)
+
+        rsi14 = TI.rsi(14)
+        rsi2 = TI.rsi(2)
+        rsi14value = float(rsi14[next(iter(rsi14))]['RSI'])
+        rsi2value = float(rsi2[next(iter(rsi2))]['RSI'])
+        rsiPrediction = round((50-rsi2value)/10,2)
+
+        writeToDB(cur, "RSI", symbol, currentPrice, rsiPrediction)
+
+        rsiEmaScore = round(emaPrediction + rsiPrediction,2)
+
+        writeToDB(cur, "EMA-RSI", symbol, currentPrice, rsiEmaScore)
+
+        # close the communication with the PostgreSQL
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
